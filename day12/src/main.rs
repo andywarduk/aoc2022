5ee0962a30b602b8cb0b42d07c594d624b3abb18
Andy Ward.uk <andy.ward.uk@gmail.com>
2022-12-12 12:19:37 +0000

    Vis day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,7 +1,11 @@
+use std::borrow::Cow;
 use std::collections::{HashSet, VecDeque};
 use std::error::Error;
+use std::fs::File;
+use std::io::{stdout, Write};
 
 use aoc::parse_input_vec;
+use gif::{Encoder, Frame, Repeat};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Get input
@@ -10,6 +14,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Run parts
     println!("Part 1: {}", part1(&map));
     println!("Part 2: {}", part2(&map));
+
+    // Visualisations
+    print!("Generating visualisations...");
+    stdout().flush()?;
+
+    part1vis(&map);
+    print!(" part 1");
+    stdout().flush()?;
+
+    part2vis(&map);
+    println!(" part2");
 
     Ok(())
 }
@@ -23,6 +38,29 @@ fn part2(map: &Map) -> usize {
     // Shortest path from END to height 0, allowed to go down by 1 only
     map.shortest_path(&map.end, |n| map.height(n) == 0, |from, to| to >= from - 1)
 }
+
+fn part1vis(map: &Map) {
+    // Shortest path from START to END, allowed to go up by 1 only
+    map.shortest_path_vis(
+        "vis/day12-1.gif",
+        &map.start,
+        |n| *n == map.end,
+        |from, to| to <= from + 1,
+    )
+}
+
+fn part2vis(map: &Map) {
+    // Shortest path from END to height 0, allowed to go down by 1 only
+    map.shortest_path_vis(
+        "vis/day12-2.gif",
+        &map.end,
+        |n| map.height(n) == 0,
+        |from, to| to >= from - 1,
+    )
+}
+
+const GIF_SCALE: u16 = 6;
+const MIN_COLOUR_COMPONENT: u8 = 32;
 
 /// Map
 struct Map {
@@ -124,32 +162,158 @@ impl Map {
         F: Fn(&Pos) -> bool,
         G: Fn(u8, u8) -> bool,
     {
+        self.shortest_path_internal(start, end_chk, neigh_chk, |_, _, _| {})
+    }
+
+    /// Calculate the shortest path from a position to a position matching a criteria with state callback
+    fn shortest_path_internal<F, G, H>(
+        &self,
+        start: &Pos,
+        end_chk: F,
+        neigh_chk: G,
+        mut state_cb: H,
+    ) -> usize
+    where
+        F: Fn(&Pos) -> bool,
+        G: Fn(u8, u8) -> bool,
+        H: FnMut(&WorkItem, &VecDeque<WorkItem>, &HashSet<Pos>),
+    {
         // Visited positions hash set
         let mut visited = HashSet::new();
         visited.insert(start.clone());
 
         // Work stack
-        let mut stack = VecDeque::new();
-        stack.push_back(WorkItem::new(start.clone(), 0));
+        let mut work_queue = VecDeque::new();
+        work_queue.push_back(WorkItem::new(start.clone(), 0));
 
         'found: loop {
             // Get next work item
-            let cur_pos = stack.pop_front().expect("No positions in the stack");
+            let cur_pos = work_queue.pop_front().expect("No positions in the stack");
+
+            // Call state callback
+            state_cb(&cur_pos, &work_queue, &visited);
+
+            // Finished?
+            if end_chk(&cur_pos.pos) {
+                break 'found cur_pos.dist;
+            }
 
             // Get neigbouring positions which match criteria
-            let neighbours = self.neighbours(&cur_pos.pos, &neigh_chk, &visited);
-
-            for n in neighbours {
-                // Finished?
-                if end_chk(&n) {
-                    break 'found cur_pos.dist + 1;
-                }
-
+            for n in self.neighbours(&cur_pos.pos, &neigh_chk, &visited) {
                 // Insert in to the visited hash map
                 visited.insert(n.clone());
-                stack.push_back(WorkItem::new(n, cur_pos.dist + 1));
+                work_queue.push_back(WorkItem::new(n, cur_pos.dist + 1));
             }
         }
+    }
+
+    fn shortest_path_vis<F, G>(&self, path: &str, start: &Pos, end_chk: F, neigh_chk: G)
+    where
+        F: Fn(&Pos) -> bool,
+        G: Fn(u8, u8) -> bool,
+    {
+        let gif_width = (self.max_x + 1) * GIF_SCALE;
+        let gif_height = (self.max_y + 1) * GIF_SCALE;
+
+        let colour_map: Vec<u8> = (0..=3)
+            .flat_map(|i| {
+                (0..26)
+                    .flat_map(|j| {
+                        let val = MIN_COLOUR_COMPONENT + (((255 - MIN_COLOUR_COMPONENT) / 26) * j);
+
+                        match i {
+                            0 => [0, val, 0],   // Green
+                            1 => [val, 0, 0],   // Red
+                            2 => [0, 0, val],   // Blue
+                            3 => [val, val, 0], // Yellow
+                            _ => unreachable!(),
+                        }
+                    })
+                    .collect::<Vec<u8>>()
+            })
+            .collect();
+
+        let mut image = File::create(path).unwrap();
+        let mut encoder = Encoder::new(&mut image, gif_width, gif_height, &colour_map).unwrap();
+
+        encoder.set_repeat(Repeat::Infinite).unwrap();
+
+        let base_frame_data = self
+            .heights
+            .iter()
+            .flat_map(|row| {
+                vec![
+                    row.iter()
+                        .flat_map(|p| [*p; GIF_SCALE as usize])
+                        .collect::<Vec<_>>();
+                    GIF_SCALE as usize
+                ]
+                .into_iter()
+                .flatten()
+                .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        let base_frame = Frame {
+            width: gif_width,
+            height: gif_height,
+            buffer: Cow::Borrowed(&*base_frame_data),
+            delay: 2,
+            ..Default::default()
+        };
+
+        encoder.write_frame(&base_frame).unwrap();
+
+        let state_cb =
+            |cur_pos: &WorkItem, work_queue: &VecDeque<WorkItem>, visited: &HashSet<Pos>| {
+                let mut frame = base_frame.clone();
+                let mut frame_data = base_frame_data.clone();
+
+                let mut colour_pixel = |x, y, col: u8| {
+                    let mut start = (y as usize * GIF_SCALE as usize * gif_width as usize)
+                        + (x as usize * GIF_SCALE as usize);
+
+                    for _ in 0..GIF_SCALE {
+                        let mut elem = start;
+
+                        for _ in 0..GIF_SCALE {
+                            frame_data[elem] = (frame_data[elem] % 26) + (col * 26);
+                            elem += 1;
+                        }
+
+                        start += gif_width as usize;
+                    }
+                };
+
+                // Colour visited blue
+                for v in visited {
+                    colour_pixel(v.x, v.y, 2);
+                }
+
+                // Colour work queue red
+                for v in work_queue {
+                    colour_pixel(v.pos.x, v.pos.y, 1);
+                }
+
+                // Colour current yellow
+                colour_pixel(cur_pos.pos.x, cur_pos.pos.y, 3);
+
+                frame.buffer = Cow::Borrowed(&frame_data);
+
+                encoder.write_frame(&frame).unwrap();
+            };
+
+        self.shortest_path_internal(start, end_chk, neigh_chk, state_cb);
+
+        // Write dummy delay frame
+        let frame = Frame {
+            width: 0,
+            height: 0,
+            delay: 1000, // 10 seconds
+            ..Frame::default()
+        };
+
+        encoder.write_frame(&frame).unwrap();
     }
 }
 
