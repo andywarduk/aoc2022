a4fa8651386de41eb549bca59de3ddf6f81c0f8d
Andy Ward.uk <andy.ward.uk@gmail.com>
2022-12-15 09:32:18 +0000

    Tidy day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -2,8 +2,10 @@ use std::{
     cmp::{max, min},
     collections::HashSet,
     error::Error,
-    str::SplitWhitespace,
 };
+
+use lazy_static::lazy_static;
+use regex::Regex;
 
 use aoc::input::parse_input_vec;
 
@@ -19,9 +21,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn part1(sensors: &[InputEnt], row: i32) -> usize {
+    // Find sensors and beacon occupying this row
     let occupied = get_occupied(sensors, row);
+
+    // Get scan range for the row
     let ranges = get_ranges(sensors, row);
 
+    // Return number of locations scanned minus locations occupied by sensors and beacons
     ranges
         .into_iter()
         .map(|(sx, ex)| (sx..=ex).filter(|x| !occupied.contains(x)).count())
@@ -29,32 +35,34 @@ fn part1(sensors: &[InputEnt], row: i32) -> usize {
 }
 
 fn part2(sensors: &[InputEnt], end_row: i32) -> i64 {
-    for row in 0..=end_row {
-        let ranges = get_ranges(sensors, row);
+    // Scan row range
+    (0..=end_row)
+        .find_map(|row| {
+            // Get scan range(s) for this row
+            let ranges = get_ranges(sensors, row);
 
-        match ranges.len() {
-            1 => (),
-            2 => {
-                if ranges[0].1 + 2 != ranges[1].0 {
-                    panic!("Gap is not 1");
+            match ranges.len() {
+                1 => None, // Single range
+                2 => {
+                    // Found a gap
+                    assert!(ranges[0].1 + 2 == ranges[1].0, "Gap should be 1");
+                    Some(((ranges[0].1 + 1) as i64 * 4_000_000) + row as i64)
                 }
-
-                return ((ranges[0].1 + 1) as i64 * 4_000_000) + row as i64;
+                _ => panic!("Ranges unexpected on row {row}: {ranges:?}"),
             }
-            _ => panic!("Ranges unexpected on row {row}: {ranges:?}"),
-        }
-    }
-
-    panic!("Beacon position not found");
+        })
+        .expect("Beacon position not found")
 }
 
 fn get_occupied(sensors: &[InputEnt], row: i32) -> HashSet<i32> {
     let mut occupied = HashSet::new();
 
+    // Get occupied positions in this row
     for s in sensors {
         if s.y == row {
             occupied.insert(s.x);
         }
+
         if s.cy == row {
             occupied.insert(s.cx);
         }
@@ -67,37 +75,41 @@ fn get_ranges(sensors: &[InputEnt], row: i32) -> Vec<(i32, i32)> {
     let mut ranges = Vec::new();
 
     for s in sensors {
+        // Find the intersection distance from the sensor to the row
         let intersect = s.dist - (row - s.y).abs();
 
         if intersect >= 0 {
+            // Range does intersect this row
+            // Work out the start and end x position if the intersection
             let mut sx = s.x - intersect;
             let mut ex = s.x + intersect;
-            let mut add = true;
 
             ranges.retain(|(esx, eex)| {
-                if sx >= *esx && ex <= *eex {
-                    add = false;
-                    return true;
-                }
-
+                // New start within this range or
+                // New end withing this range or
+                // This range right after new range or
+                // New range right after this range or
+                // New range contained in this range
                 if *esx >= sx && *esx <= ex
                     || *eex >= sx && *eex <= ex
                     || *esx == ex + 1
                     || *eex + 1 == sx
+                    || sx >= *esx && ex <= *eex
                 {
+                    // Calculate new combined range
                     sx = min(sx, *esx);
                     ex = max(ex, *eex);
+
+                    // Don't retain this range
                     false
                 } else {
+                    // Retain this range
                     true
                 }
             });
 
-            if add {
-                ranges.push((sx, ex));
-            }
-
-            ranges.sort();
+            // Add new range
+            ranges.push((sx, ex));
         }
     }
 
@@ -118,34 +130,30 @@ struct Sensor {
 type InputEnt = Sensor;
 
 fn input_transform(line: String) -> InputEnt {
-    let mut iter = line.split_whitespace();
+    lazy_static! {
+        static ref RE: Regex = Regex::new(
+            r"^Sensor at x=(-?\d*), y=(-?\d*): closest beacon is at x=(-?\d*), y=(-?\d*)$"
+        )
+        .unwrap();
+    }
 
-    let parse = |iter: &mut SplitWhitespace| {
-        iter.next()
-            .unwrap()
-            .split('=')
-            .nth(1)
-            .unwrap()
-            .split(',')
-            .next()
-            .unwrap()
-            .split(':')
-            .next()
-            .unwrap()
-            .parse::<i32>()
-            .unwrap()
-    };
+    let nums: Vec<i32> = RE
+        .captures(&line)
+        .unwrap_or_else(|| panic!("Invalid input line: {line}"))
+        .iter()
+        .skip(1)
+        .map(|m| {
+            m.expect("No match")
+                .as_str()
+                .parse::<i32>()
+                .expect("Invalid number")
+        })
+        .collect();
 
-    iter.next().unwrap();
-    iter.next().unwrap();
-    let x = parse(&mut iter);
-    let y = parse(&mut iter);
-    iter.next().unwrap();
-    iter.next().unwrap();
-    iter.next().unwrap();
-    iter.next().unwrap();
-    let cx = parse(&mut iter);
-    let cy = parse(&mut iter);
+    let x = nums[0];
+    let y = nums[1];
+    let cx = nums[2];
+    let cy = nums[3];
 
     let dx = (x - cx).abs();
     let dy = (y - cy).abs();
