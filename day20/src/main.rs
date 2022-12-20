46e14e37445cebb48b36607078a96fdd22625b04
Andy Ward.uk <andy.ward.uk@gmail.com>
2022-12-20 23:51:43 +0000

    Day 20 tidy

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -14,51 +14,65 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn part1(input: &[i64]) -> i64 {
+    // Transform input
     let input = input
         .iter()
         .enumerate()
-        .map(|(i, n)| (i, *n))
+        .map(|(i, &n)| (i, n))
         .collect::<Vec<_>>();
 
+    // Create work vector from input
     let mut vec = input.to_vec();
 
+    // Mix it up
     mix(&input, &mut vec);
 
+    // Calculate result
     elem(&vec, 1000) + elem(&vec, 2000) + elem(&vec, 3000)
 }
 
 fn part2(input: &[i64]) -> i64 {
+    // Transform input
     let input = input
         .iter()
         .enumerate()
-        .map(|(i, n)| (i, *n * 811589153))
+        .map(|(i, &n)| (i, n * 811589153))
         .collect::<Vec<_>>();
 
+    // Create work vector from input
     let mut vec = input.to_vec();
 
+    // Loop 10 times
     for _ in 0..10 {
+        // Mix it up
         mix(&input, &mut vec);
     }
 
+    // Calculate result
     elem(&vec, 1000) + elem(&vec, 2000) + elem(&vec, 3000)
 }
 
 fn mix(input: &[(usize, i64)], vec: &mut Vec<(usize, i64)>) {
     for val in input {
-        let pos = vec.iter().position(|v| *v == *val).unwrap();
+        // Get vector position of the number
+        let old_pos = vec.iter().position(|v| *v == *val).unwrap();
 
-        let ent = vec.remove(pos);
+        // Remove it
+        let ent = vec.remove(old_pos);
 
-        vec.insert(
-            (pos as isize + ent.1 as isize).rem_euclid(vec.len() as isize) as usize,
-            ent,
-        );
+        // Calculate new position
+        let new_pos = (old_pos as isize + ent.1 as isize).rem_euclid(vec.len() as isize) as usize;
+
+        // Insert it in to the new position
+        vec.insert(new_pos, ent);
     }
 }
 
 fn elem(vec: &[(usize, i64)], idx: usize) -> i64 {
+    // Get position of the zero
     let pos = vec.iter().position(|(_, v)| *v == 0).unwrap();
 
+    // Return nth element from zero
     vec[(pos + idx) % vec.len()].1
 }
 
@@ -87,6 +101,6 @@ mod tests {
     fn test1() {
         let input = parse_test_vec(EXAMPLE1, input_transform).unwrap();
         assert_eq!(part1(&input), 3);
-        assert_eq!(part2(&input), 0 /* TODO */);
+        assert_eq!(part2(&input), 1623178306);
     }
 }
